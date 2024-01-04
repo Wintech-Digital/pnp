caba97ee89150e05848a4874f2beba1246861dc4
mdolzhko <m.dolzhko@gmail.com>
2024-01-04 17:22:11 +0200

    update slider;

--- src/api/endpoint.ts
+++ src/api/endpoint.ts
@@ -2,9 +2,9 @@ import { PageId } from "@constants";
 
 enum ComponentQuery {
     HERO = 'populate[hero][populate][btn][populate]=*&populate[hero][populate][coverImage][populate]=*',
-    SLOTS_LIST = 'populate[slotsList][populate]=*'
+    OFFERS = 'populate[offerSlider][populate][offer][populate]=*',
 }
 
 export enum PageEndPoint {
-    HOME = `page-${PageId.HOME}?${ComponentQuery.HERO}`,
+    HOME = `page-${PageId.HOME}?${ComponentQuery.HERO}&${ComponentQuery.OFFERS}`,
 }
--- src/components/slider/SliderItem.tsx
+++ src/components/slider/SliderItem.tsx
@@ -1,11 +1,13 @@
-import type { FC, ReactNode } from "react";
-import type { GetImageResult } from "astro";
+import { type FC, useState, useEffect } from "react";
+import { getImage } from 'astro:assets';
 import ButtonRounded from "@components/buttons/ButtonRounded";
+const convert = async (url) => await getImage({ src: url, width: 144, height: 172 });
 
 interface Props {
   text: string;
   description: string;
-  pic: GetImageResult,
+  pic: string,
+  ext: string
 }
 const blockFlex = "flex flex-col-reverse items-center md:flex-row";
 const blockSizes = "rounded-[24px] px-[16px] py-[16px]";
@@ -16,6 +18,14 @@ const blockBg = "bg-pnp/[.05] hover:gradient-green";
 const textBlockFlex = "flex flex-col items-start justify-between";
 
 const SliderItem: FC<Props> = ({ ...props }) => {
+  const [pic, setPic] = useState('');
+  useEffect(()=>{
+    if(props.ext === '.png'){
+      convert(props.pic).then(p=> setPic(p.src))
+    }
+    setPic(props.pic);
+  },[props.pic, props.ext])
+  console.log('pic', pic)
   return (
     <div className={`w-full h-full`}>
       <div className={`h-full ${blockBgTransition} ${blockBg} ${blockFlex} ${blockSizes} ${blockBorder} ${blockSizesMd}`}>
@@ -35,7 +45,10 @@ const SliderItem: FC<Props> = ({ ...props }) => {
         </div>
         <div
           className="w-full grid justify-items-start mb-[24px] md:justify-items-end md:mb-[0px] h-[172px]">
-            <img src={props.pic.src} alt="" />
+            {pic && 
+              <img src={pic} width='144' height='172' alt="" />
+            }
+            {/* <img src={props.pic} width='144' height='172' alt="" /> */}
         </div>
       </div>
     </div>
