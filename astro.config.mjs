ace65ff1f79b28c4aac67f021aba6fa3856f5fa3
mdolzhko <m.dolzhko@gmail.com>
2024-01-24 20:53:54 +0200

    add sitemap generation;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,21 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// import sitemap from '@astrojs/sitemap'; //integrations: [ sitemap()];  
+import sitemap from '@astrojs/sitemap';  
 import react from "@astrojs/react";
 import tailwind from '@astrojs/tailwind';
 import { defineConfig, squooshImageService } from 'astro/config';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// output: 'static',
 export default defineConfig({
+  output: 'static',
 
+  site: 'https://pnp-a5kn.vercel.app',
   integrations: [
     react(),
     tailwind(),
+    sitemap()
   ],
 
   image: {
