3293e7c06b4b4d137ec160a9bdbae3fda5c01eca
mdolzhko <m.dolzhko@gmail.com>
2024-01-28 20:14:52 +0200

    add bonus slider assests;

--- src/components/slider/SliderItem2.tsx
+++ src/components/slider/SliderItem2.tsx
@@ -1,47 +1,41 @@
-import ButtonRounded from "@components/buttons/ButtonRounded";
+import ButtonRounded from '@components/buttons/ButtonRounded';
 
 const bg = 'bg-pnp/[.03]';
 const border = 'border border-pnp/[.1]';
 const shadow = 'shadow-2xl';
 const gradient = 'gradient-red';
 const rounded = 'rounded-[24px] sm:rounded-[32px]';
-// const flex = 'flex flex-col justify-between';
 const common = ` ${rounded} ${bg} ${gradient} ${shadow} ${border}`;
-const SliderItem2 = ({ ...props}) => { 
+const SliderItem2 = ({ ...props }) => {
   return (
     <div className={`h-full overflow-hidden ${common}`}>
-      <a href={props.imageLink}>
-        <img
-          src={props.imageData.url}
-          width={props.imageData.w}
-          height={props.imageData.h}
-          alt={props.imageData.alternativeText || ''}
-          className="w-full"
-        />
-      </a>
+      <img
+        src={props.imageData.url}
+        width={props.imageData.w}
+        height={props.imageData.h}
+        alt={props.imageData.alternativeText || ''}
+        className="w-full"
+      />
       <div className="py-[24px] px-[24px] h-[250px] sm:h-[180px] flex flex-col justify-between">
-          <div className="text-pnp/[.6] text-base font-semibold">
-              {props.text}
-          </div>
-          <div className='text-[28px] font-bold leading-[34px] mb-6'>
-            {props.description}
-          </div>
-          <div className="flex flex-col-reverse sm:flex-row sm:items-center gap-4">
-            {props.buttons.map((item, index) =>{
-              return (
-                  <ButtonRounded
-                    key={index}
-                    size="lg"
-                    color={item?.type || 'red'}
-                    title={item?.name || ''}
-                    link={item?.link || '#'}
-                    className="w-auto"
-                  />
-              )
-            })}
-          </div>
+        <div className="text-pnp/[.6] text-base font-semibold">{props.text}</div>
+        <div className="text-[28px] font-bold leading-[34px] mb-6">{props.description}</div>
+        <div className="flex flex-col-reverse sm:flex-row sm:items-center gap-4">
+          {props.buttons.map((item, index) => {
+            return (
+              <ButtonRounded
+                key={index}
+                size="lg"
+                color={item?.type || 'red'}
+                title={item?.name || ''}
+                link={item?.link || '#'}
+                className="w-auto"
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
-)};
+  );
+};
 
 export default SliderItem2;
