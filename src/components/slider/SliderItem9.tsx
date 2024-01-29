a7d1e661e311ef68a70bb426d36354c0b1811af5
mdolzhko <m.dolzhko@gmail.com>
2024-01-29 04:00:10 +0200

    alts for app page;

--- src/components/slider/SliderItem9.tsx
+++ src/components/slider/SliderItem9.tsx
@@ -3,7 +3,7 @@ import type { ImageMetadata } from "astro";
 
 interface Props {
   text: string;
-  pic: string;
+  pic: any;
   description: string;
 }
 
@@ -12,12 +12,13 @@ const SliderItem5: FC<Props> = ({ ...props }) => {
   const rounded = 'rounded-[24px] sm:rounded-[32px]';
   const paddings = 'py-[24px] px-[24px]';
   const flexBox = 'flex flex-col gap-2';
+
   return (
     <div
       className={`relative w-full h-full ${flexBox} card-base text-center border bg-pnp/[.03] hover:gradient-green-coner-1 ${rounded} ${paddings}`}
     >
 
-      <img src={pic} alt="imagepng." className={""} />
+      <img src={pic.url} alt={pic.alternativeText}/>
       <div className="text-28-34-700 text-pnp mb-4">{text}</div>
       <div className="text-16-24-400 text-pnp-grey20">{description}</div>
     </div>
